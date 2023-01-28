d8d56adbc28702924e66dc3c6619207b492765f2
Roman Danylenko <ramonvicious@gmail.com>
2023-01-29 01:07:46 +0300

    Authorization added Post, User, Tag, LookUp and Comment Models rules and relations added

--- protected/models/Comment.php
+++ protected/models/Comment.php
@@ -13,101 +13,96 @@
  * @property string $url
  * @property integer $post_id
  */
-class Comment extends CActiveRecord
-{
-	/**
-	 * @return string the associated database table name
-	 */
-	public function tableName()
-	{
-		return '{{comment}}';
-	}
+class Comment extends CActiveRecord {
+    const STATUS_PENDING = 1;
+    const STATUS_APPROVED = 2;
 
-	/**
-	 * @return array validation rules for model attributes.
-	 */
-	public function rules()
-	{
-		// NOTE: you should only define rules for those attributes that
-		// will receive user inputs.
-		return array(
-			array('author, content, status', 'required'),
-			array('status, post_id', 'numerical', 'integerOnly'=>true),
-			array('author, content, email, create_time, url', 'length', 'max'=>128),
-			// The following rule is used by search().
-			// @todo Please remove those attributes that should not be searched.
-			array('id, author, content, status, email, create_time, url, post_id', 'safe', 'on'=>'search'),
-		);
-	}
+    /**
+     * @return string the associated database table name
+     */
+    public function tableName() {
+        return '{{comment}}';
+    }
 
-	/**
-	 * @return array relational rules.
-	 */
-	public function relations()
-	{
-		// NOTE: you may need to adjust the relation name and the related
-		// class name for the relations automatically generated below.
-		return array(
-		);
-	}
+    /**
+     * @return array validation rules for model attributes.
+     */
+    public function rules() {
+        // NOTE: you should only define rules for those attributes that
+        // will receive user inputs.
+        return array(
+            array('author, content, status', 'required'),
+            array('status, post_id', 'numerical', 'integerOnly' => true),
+            array('author, content, email, create_time, url', 'length', 'max' => 128),
+            // The following rule is used by search().
+            // @todo Please remove those attributes that should not be searched.
+            array('id, author, content, status, email, create_time, url, post_id', 'safe', 'on' => 'search'),
+        );
+    }
 
-	/**
-	 * @return array customized attribute labels (name=>label)
-	 */
-	public function attributeLabels()
-	{
-		return array(
-			'id' => 'ID',
-			'author' => 'Author',
-			'content' => 'Content',
-			'status' => 'Status',
-			'email' => 'Email',
-			'create_time' => 'Create Time',
-			'url' => 'Url',
-			'post_id' => 'Post',
-		);
-	}
+    /**
+     * @return array relational rules.
+     */
+    public function relations() {
+        // NOTE: you may need to adjust the relation name and the related
+        // class name for the relations automatically generated below.
+        return array();
+    }
 
-	/**
-	 * Retrieves a list of models based on the current search/filter conditions.
-	 *
-	 * Typical usecase:
-	 * - Initialize the model fields with values from filter form.
-	 * - Execute this method to get CActiveDataProvider instance which will filter
-	 * models according to data in model fields.
-	 * - Pass data provider to CGridView, CListView or any similar widget.
-	 *
-	 * @return CActiveDataProvider the data provider that can return the models
-	 * based on the search/filter conditions.
-	 */
-	public function search()
-	{
-		// @todo Please modify the following code to remove attributes that should not be searched.
+    /**
+     * @return array customized attribute labels (name=>label)
+     */
+    public function attributeLabels() {
+        return array(
+            'id' => 'ID',
+            'author' => 'Author',
+            'content' => 'Content',
+            'status' => 'Status',
+            'email' => 'Email',
+            'create_time' => 'Create Time',
+            'url' => 'Url',
+            'post_id' => 'Post',
+        );
+    }
 
-		$criteria=new CDbCriteria;
+    /**
+     * Retrieves a list of models based on the current search/filter conditions.
+     *
+     * Typical usecase:
+     * - Initialize the model fields with values from filter form.
+     * - Execute this method to get CActiveDataProvider instance which will filter
+     * models according to data in model fields.
+     * - Pass data provider to CGridView, CListView or any similar widget.
+     *
+     * @return CActiveDataProvider the data provider that can return the models
+     * based on the search/filter conditions.
+     */
+    public function search() {
+        // @todo Please modify the following code to remove attributes that should not be searched.
 
-		$criteria->compare('id',$this->id);
-		$criteria->compare('author',$this->author,true);
-		$criteria->compare('content',$this->content,true);
-		$criteria->compare('status',$this->status);
-		$criteria->compare('email',$this->email,true);
-		$criteria->compare('create_time',$this->create_time,true);
-		$criteria->compare('url',$this->url,true);
-		$criteria->compare('post_id',$this->post_id);
+        $criteria = new CDbCriteria;
 
-		return new CActiveDataProvider($this, array(
-			'criteria'=>$criteria,
-		));
-	}
+        $criteria->compare('id', $this->id);
+        $criteria->compare('author', $this->author, true);
+        $criteria->compare('content', $this->content, true);
+        $criteria->compare('status', $this->status);
+        $criteria->compare('email', $this->email, true);
+        $criteria->compare('create_time', $this->create_time, true);
+        $criteria->compare('url', $this->url, true);
+        $criteria->compare('post_id', $this->post_id);
 
-	/**
-	 * Returns the static model of the specified AR class.
-	 * Please note that you should have this exact method in all your CActiveRecord descendants!
-	 * @param string $className active record class name.
-	 * @return Comment the static model class
-	 */
-	public static function model($className=__CLASS__)
-	{
-		return parent::model($className);
-	}
+        return new CActiveDataProvider($this, array(
+            'criteria' => $criteria,
+        ));
+    }
+
+    /**
+     * Returns the static model of the specified AR class.
+     * Please note that you should have this exact method in all your CActiveRecord descendants!
+     * @param string $className active record class name.
+     * @return Comment the static model class
+     */
+    public static function model($className = __CLASS__) {
+        return parent::model($className);
+    }
 }
